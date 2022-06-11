3f4b8f697c52f0cb70cc9ec0236355775619456d
Yousof <yousof.buali@gmail.com>
2022-06-11 11:37:34 +0430

    feat(config): added config.js

---
+++ js/config/config.js
@@ -0,0 +1,17 @@
+
+/**
+ * @constant
+ * Contains necessary strings and information to work with https://swapi.dev.
+ */
+const swapi = {
+  url: {
+    base: "https://swapi.dev/api",
+    films: "films",
+    people: "people",
+    planets: "planets",
+    starships: "starships",
+    vehicles: "vehicles"
+  }
+};
+
+module.exports = {swapi};
